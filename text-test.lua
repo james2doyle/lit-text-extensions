21717c8d793c5d28c1b209cc46e08dce9f9b5dea
james2doyle <james2doyle@gmail.com>
2016-02-06 14:48:09 -0800

    refactor for new lit. update test. bump version

--- text-test.lua
+++ text-test.lua
@@ -1,7 +1,10 @@
-local extensions = require('./init').list
+local extensions = require('./init')
 
 for i = 1, #extensions do
   if 'txt' == extensions[i] then
     print('txt is a valid extension')
+  end
+  if 'md' == extensions[i] then
+    print('md is a valid extension')
   end
 end
